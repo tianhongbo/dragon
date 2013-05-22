8f652fd66d8e255c8d9cf02359803f0c09e437ea
tianhongbo <tianhongbo1@gmail.com>
2013-05-21 18:52:59 -0700

    First Class

--- JsonClient.py
+++ JsonClient.py
@@ -1,40 +1,16 @@
-# Before you access any OpenStack API, you should obtain the token from KeyStone.
-# This class is that Using Python to Obtain the Authentication Token.
-# This token you got will expire in 24 hours which you need to consider.
-# The following are the auth information:
-# export OS_TENANT_NAME=admin
-# export OS_USERNAME=admin
-# export OS_PASSWORD=password
-# export OS_AUTH_URL="http://localhost:5000/v2.0/"
-# export SERVICE_ENDPOINT="http://localhost:35357/v2.0"
-# export SERVICE_TOKEN=password
+# This function will call the quantum API# 
 # QuantumHost = "http://10.145.90.128:9696/v2.0/api/"
 
 import httplib
 import json
 import urllib
 import pprint
+import KeyStoneToken
 
-## Obtain the token from KeyStone for user - admin
-# arguments
-url = "10.145.90.128:5000"
-params = '{"auth":{"passwordCredentials":{"username":"admin", "password":"password"},"tenantId":"f540d68440e146ed83537bb88002af16"}}'
-headers = {"Content-Type": "application/json"}
-
-# HTTP connetion
-conn = httplib.HTTPConnection(url)
-conn.request("POST", "/v2.0/tokens", params, headers)
-
-# HTTP response
-response = conn.getresponse()
-data = response.read()
-dd = json.loads(data)
-
-# HTTP close
-conn.close()
-
-# Display the token
-apitoken = dd['access']['token']['id']
+# Get the token from keystone via API
+Token = KeyStoneToken.KeyStoneToken()
+Token.GetToken()
+apitoken = Token.value
 print "Your token is: %s" % apitoken
 
 ## List the networks configured in Quantum by user - Admin
@@ -52,7 +28,7 @@ dataQuantum = responseQuantum.read()
 ddQuantum = json.loads(dataQuantum)
 
 # HTTP Close
-conn.close()
+connQuantum.close()
 
 # Display the data
 print "This is the original data:", ddQuantum
---
+++ KeyStoneToken.py
@@ -0,0 +1,44 @@
+# Before you access any OpenStack API, you should obtain the token from KeyStone.
+# This class is that Using Python to Obtain the Authentication Token.
+# This token you got will expire in 24 hours which you need to consider.
+# The following are the auth information:
+# export OS_TENANT_NAME=admin
+# export OS_USERNAME=admin
+# export OS_PASSWORD=password
+# export OS_AUTH_URL="http://localhost:5000/v2.0/"
+# export SERVICE_ENDPOINT="http://localhost:35357/v2.0"
+# export SERVICE_TOKEN=password
+
+import httplib
+import json
+import urllib
+
+class KeyStoneToken:
+
+    # Token value, initiled with none
+    value = ''
+
+    def __init__(self):
+        # All the arguments for user - Admin
+        # If you want to change to another user, please modify the self.params
+        self.url = "10.145.90.128:5000"
+        self.params = '{"auth":{"passwordCredentials":{"username":"admin", "password":"password"},"tenantId":"f540d68440e146ed83537bb88002af16"}}'
+        self.headers = {"Content-Type": "application/json"}
+        
+    # Method to obtain the token from KeyStone
+    def GetToken(self):
+        
+        # HTTP connetion
+        conn = httplib.HTTPConnection(self.url)
+        conn.request("POST", "/v2.0/tokens", self.params, self.headers)
+        
+        # HTTP response
+        response = conn.getresponse()
+        data = response.read()
+        dd = json.loads(data)
+        
+        # HTTP close
+        conn.close()
+        
+        # Display the token
+        self.value = dd['access']['token']['id']
