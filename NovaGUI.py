6faae9816d3bc52101aecbeaabc7f5fdf1e02597
tianhongbo <tianhongbo1@gmail.com>
2013-05-29 22:06:38 -0700

    add nova GUI

---
+++ NovaGUI.py
@@ -0,0 +1,28 @@
+# Created by Hongbo Tian at May 29, 2013
+# Purpose: Demo show the OpenStack capability
+# This module focus on Nova component in OpenStack
+
+
+from Tkinter import *
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print "hi there, everyone!"
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
